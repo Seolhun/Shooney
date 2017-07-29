640956ef47aac45d4ae9ad16bd0a3cfd40826668
shun10114@gmail.com <shun10114@gmail.com>
2017-07-29 14:32:46 +0900

    modification for auto build

---
+++ src/test/java/com/shooney/test/SurveyChartTest.java
@@ -0,0 +1,59 @@
+package com.imedisyn.brain.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import tunner.hospital.service.HospitalService;
+import tunner.survey.model.Survey;
+import tunner.survey.service.SurveyService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/config/action-servlet.xml", "classpath*:config/spring/context-*.xml" })
+public class SurveyChartTest {
+	private static final Logger log = Logger.getLogger(SurveyChartTest.class);
+
+	@Autowired
+	private SurveyService surveyService;
+	@Autowired
+	private HospitalService hospitalService;
+	
+
+//	@Before
+//	public void setup() {
+//		// Create ServiceObject and save to DB
+//	}
+
+	@Test
+	public void selectHospitalDetail() throws Exception {
+		log.info("param : selectSurveyListNonCache");
+		Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("sessionEmail", "hyunyoul1004@imedisyn.com");
+		paramMap.put("sessionUserName", "관리자");
+		paramMap.put("sessionHospitalIdx", "104-86-46675");
+		paramMap.put("sessionHospitalNm", "아이메디신");
+		paramMap.put("surveyBindingId", 34);
+		
+		Survey survey = new Survey();
+		survey.setType(1);
+		for (int i = 0; i < 100; i++) {
+			Map<String, Object> hospitalDetail = hospitalService.hospitalDetail(paramMap); //병원 정보 조회
+			Assert.assertSame(hospitalDetail, hospitalDetail);
+		}
+	}
+
+	
+//	@After
+//	public void teardown() {
+//		// Delete created ServiceObject from DB
+//	}
+}
---
+++ src/test/java/com/shooney/test/SurveyControllerTest.java
@@ -0,0 +1,63 @@
+//package com.imedisyn.brain.test;
+//
+//import static org.hamcrest.CoreMatchers.instanceOf;
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.junit.Assert.assertThat;
+//
+//import org.apache.log4j.Logger;
+//import org.junit.After;
+//import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//import org.springframework.test.context.web.WebAppConfiguration;
+//
+//import tunner.survey.model.Survey;
+//import tunner.survey.service.SurveyService;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@WebAppConfiguration
+//@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/config/action-servlet.xml", "classpath*:config/spring/context-*.xml" })
+//public class SurveyControllerTest {
+//	private static final Logger log = Logger.getLogger(SurveyControllerTest.class);
+//
+//	@Autowired
+//	private SurveyService surveyService;
+//
+//	@Before
+//	public void setup() {
+//		// Create ServiceObject and save to DB
+//	}
+//
+//	@Test
+//	public void selectSurveyListNonCache() throws Exception {
+//		log.info("param : selectSurveyListNonCache");
+//		assertThat(surveyService, instanceOf(SurveyService.class));
+//
+//		Survey survey = new Survey();
+//		survey.setType(1);
+//		for (int i = 0; i < 100; i++) {
+//			Assert.assertThat(surveyService.selectSurveyList(survey), is(surveyService.selectSurveyList(survey)));
+//		}
+//	}
+//
+//	@Test
+//	public void selectSurveyListCache() throws Exception {
+//		log.info("param : selectSurveyListCache");
+//		assertThat(surveyService, instanceOf(SurveyService.class));
+//
+//		Survey survey = new Survey();
+//		survey.setType(1);
+//		for (int i = 0; i < 100; i++) {
+//			Assert.assertThat(surveyService.selectSurveyList(survey), is(surveyService.selectSurveyList(survey)));
+//		}
+//	}
+//	
+//	@After
+//	public void teardown() {
+//		// Delete created ServiceObject from DB
+//	}
+//}
